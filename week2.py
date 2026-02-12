ade7e7451edbd15d75592a34c84c9c04f2b5e43d
elijahrafael <palimaelijahrafael@gmail.com>
2026-02-12 23:25:17 +0800

    Add files via upload

---
+++ week2.py
@@ -0,0 +1,17 @@
+fullname = "Elijah Rafael A. Palima"
+age = 20
+height_in_feet = 5.5
+height_in_meter = height_in_feet * 0.3048
+currently_working = False
+
+print("Fullname: ", fullname)
+print(type(fullname))
+
+print("Age: ", age)
+print(type(age))
+
+print("Height in meters: ", height_in_meter)
+print(type(height_in_meter))
+
+print("Currently working? ", currently_working)
+print(type(currently_working))
